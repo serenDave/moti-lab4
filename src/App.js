5a42b74ef3ea7ba11082ec049a3d8448999e9d0b
Denis <1denrocks1@gmail.com>
2020-12-11 21:08:52 +0100

    added matrix display

--- src/App.js
+++ src/App.js
@@ -6,16 +6,140 @@ import Player from './components/Player/Player';
 const initialState = {
     gameState: 'not-started',
     strategy: null,
-    strategyId: 0,
+    multiplier: 1,
+    opponentMultiplier: 1,
     opponentStrategy: null,
-    opponentStrategyId: 0,
     finalResult: null,
+    matrix: null,
 }; 
 
 class App extends Component {
-    state = initialState;
 
+    state = initialState;
     previousTableCell = null;
+
+    componentDidMount() {
+        this.initiateMatrix();
+    }
+
+    initiateMatrix() {
+        let matrix = [];
+
+        for (let i = 1; i <= 4; i++) {
+            matrix[i - 1] = [];
+
+            for (let j = 1; j <= 4; j++) {
+                switch (i) {
+                    case 1:
+                        switch (j) {
+                            case 1:
+                                matrix[i - 1][j - 1] = 0;
+                                break;
+                            case 2:
+                                matrix[i - 1][j - 1] = 2;
+                                break;
+                            case 3:
+                                matrix[i - 1][j - 1] = -3;
+                                break;
+                            case 4:
+                                matrix[i - 1][j - 1] = 0;
+                                break;
+                            default:
+                                break;
+                        }
+                        break;
+                    case 2:
+                        switch (j) {
+                            case 1:
+                                matrix[i - 1][j - 1] = -2;
+                                break;
+                            case 2:
+                                matrix[i - 1][j - 1] = 0;
+                                break;
+                            case 3:
+                                matrix[i - 1][j - 1] = 0;
+                                break;
+                            case 4:
+                                matrix[i - 1][j - 1] = 3;
+                                break;        
+                            default:
+                                break;
+                        }
+                        break;
+                    case 3:
+                        switch (j) {
+                            case 1:
+                                matrix[i - 1][j - 1] = 3;
+                                break;
+                            case 2:
+                                matrix[i - 1][j - 1] = 0;
+                                break;
+                            case 3:
+                                matrix[i - 1][j - 1] = 0;
+                                break;
+                            case 4:
+                                matrix[i - 1][j - 1] = -4;
+                                break;        
+                            default:
+                                break;
+                        }
+                        break;
+                    case 4:
+                        switch (j) {
+                            case 1:
+                                matrix[i - 1][j - 1] = 0;
+                                break;
+                            case 2:
+                                matrix[i - 1][j - 1] = -3;
+                                break;
+                            case 3:
+                                matrix[i - 1][j - 1] = 4;
+                                break;
+                            case 4:
+                                matrix[i - 1][j - 1] = 0;
+                                break;
+                            default:
+                                break;
+                        }
+                        break;            
+                    default:
+                        break;
+                }
+            }
+        }
+
+        this.setState({ matrix });
+    }
+
+    showMatrix() {
+        this.state.matrix.forEach((s1, i) => {
+            s1.forEach((s2, j) => {
+                const situationElem = document.getElementById(`${i + 1}${j + 1}`);
+                situationElem.textContent = s2;
+                situationElem.style.backgroundColor = s2 > 0 ? 'green' : s2 < 0 ? 'red' : 'grey';
+            });
+        });
+    }
+
+    getOptimalStrategy() {
+        const mins = {};
+
+        this.state.matrix.forEach((strategy, i) => {
+            let min = Math.min(...strategy);
+            mins[i] = min;
+        });
+
+        const max = Math.max(...Object.values(mins));
+
+        let optimal;
+        for (const strategy in mins) {
+            if (mins[strategy] === max) {
+                optimal = strategy;
+            }
+        }
+
+        return +optimal + 1;
+    }
 
     clearPreviousTableCell() {
         if (this.previousTableCell) {
@@ -35,13 +159,15 @@ class App extends Component {
 
     getResults() {
         let score = 0;
+        let points = 0;
         let opponentsScore = 0;
+        let opponentsPoints = 0;
 
         // First checking our scores
         if (this.state.strategy[1] === 1 && this.state.opponentStrategy[0] === 1) {
             score++;
         } else if (this.state.strategy[1] === 2 && this.state.opponentStrategy[0] === 2) {
-            score ++
+            score++;
         }
         
         // Then opponents scores
@@ -55,8 +181,10 @@ class App extends Component {
 
         if (score > opponentsScore) {
             result = 'win';
+            points += this.state.opponentStrategy[1] * this.state.opponentMultiplier;
         } else if (opponentsScore > score) {
             result = 'lose';
+            opponentsPoints += this.state.strategy[1] * this.state.multiplier;
         } else {
             result = 'draw';
         }
@@ -68,7 +196,7 @@ class App extends Component {
 
         const tableCell = document.getElementById(`${strategyId}${opponentStrategyId}`);
 
-        tableCell.textContent = result === 'win' ? score : result === 'lose' ? -opponentsScore : 0;
+        tableCell.textContent = result === 'win' ? points : result === 'lose' ? -opponentsPoints : 0;
         tableCell.style.backgroundColor = result === 'win' ? 'green' : result === 'lose' ? 'red' : 'grey';
         tableCell.style.color = '#fff';
 
@@ -79,7 +207,11 @@ class App extends Component {
 
     startGame() {
         this.clearPreviousTableCell();
-        this.setState({ ...initialState });
+
+        const startState = { ...initialState };
+        delete startState.matrix;
+
+        this.setState({ ...startState });
     }
 
     getStrategyId(strategy) {
@@ -192,7 +324,21 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <header>Гра в пальці</header>
+                <header>
+                    <p>Гра в пальці</p>
+                    <ul>
+                        <li style={{ marginRight: '20px' }}>
+                            <Button variant="warning" onClick={() => {
+                                const strategy = this.getOptimalStrategy();
+                                const strategyElem = document.getElementById(`strategy-${strategy}`);
+                                strategyElem.style.backgroundColor = '#ffc107';
+                            }}>Показати оптимальну стратегію</Button>
+                        </li>
+                        <li>
+                            <Button variant="primary" onClick={() => this.showMatrix()}>Показати матрицю гри</Button>
+                        </li>
+                    </ul>
+                </header>
                 <main>
                     <div className="container-custom">
                         <Player result={this.state.finalResult} />
